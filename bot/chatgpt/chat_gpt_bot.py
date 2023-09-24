55de18e3a5a5b892e37a9363690259d4a51d999f
Tianshuang Ke <qinyun575@gmail.com>
2023-09-24 09:50:37 +0800

    Fix Azure api paramter[engine]

--- bot/chatgpt/chat_gpt_bot.py
+++ bot/chatgpt/chat_gpt_bot.py
@@ -33,6 +33,7 @@ class ChatGPTBot(Bot, OpenAIImage):
 
         self.sessions = SessionManager(ChatGPTSession, model=conf().get("model") or "gpt-3.5-turbo")
         self.args = {
+            "engine": conf().get("open_ai_api_engine") or "davinci",
             "model": conf().get("model") or "gpt-3.5-turbo",  # 对话模型的名称
             "temperature": conf().get("temperature", 0.9),  # 值在[0,1]之间，越大表示回复越具有不确定性
             # "max_tokens":4096,  # 回复最大的字符数
